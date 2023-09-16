1d46d2793a091f135014679e56feef6683ce022f
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-09-16 17:32:41 +0530

    reduce

--- reducePolyfill.js
+++ reducePolyfill.js
@@ -1,25 +1,44 @@
-const array1 = [1, 2, 3, 4];
+// const array1 = [1, 2, 3, 4];
 
-// 0 + 1 + 2 + 3 + 4
-const initialValue = 0;
-const sumWithInitial = array1.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue);
+// // 0 + 1 + 2 + 3 + 4
+// const initialValue = 0;
+// const sumWithInitial = array1.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue);
 
-console.log(sumWithInitial);
-// Expected output: 10
+// console.log(sumWithInitial);
+// // Expected output: 10
 
 
-//polyfill for reduce
+// //polyfill for reduce
+// Array.prototype.customReduce = function(callback, initialValue){
+//     let accumulator = initialValue
+//     for (let i = 0; i< this.length; i++){
+//         accumulator = accumulator ? callback(accumulator, this[i], i,this) : this[i]
+  
+//     }
+//     return accumulator
+// }
+
+// let checkReduce = array1.customReduce((acc,curr, i, arr)=>{
+//     return acc + curr
+// },0)
+
+// console.log(`custom reduce method ${checkReduce}`)
+
+//polyfill two
+
+const sampleArr = [12,45,1,4,3]
+
 Array.prototype.customReduce = function(callback, initialValue){
     let accumulator = initialValue
-    for (let i = 0; i< this.length; i++){
-        accumulator = accumulator ? callback(accumulator, this[i], i,this) : this[i]
-  
+    for(let i=0; i< this.length; i++){
+        accumulator = accumulator? callback(accumulator, this[i], i,this) : this[i]
     }
     return accumulator
 }
 
-let checkReduce = array1.customReduce((acc,curr, i, arr)=>{
-    return acc + curr
-},0)
+let resultArr = sampleArr.customReduce((acc, curr, i, arr)=>{
+    return acc - curr
 
-console.log(`custom reduce method ${checkReduce}`)
+})
+
+console.log(resultArr);
