791ffb85a354a715c7cf48cc613ceefad5fa509e
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-09-17 20:52:30 +0530

    new

---
+++ prototype.js
@@ -0,0 +1,11 @@
+function multiplyByFive(num){
+    return num*5
+
+}
+
+// multiplyByFive.power = 2
+
+
+console.log(multiplyByFive(5))
+console.log(multiplyByFive.power)
+console.log(multiplyByFive.prototype)
